ec07c7f7c459e36cf4608440a855eaec7eb55db9
Partha B baruah <parthabb@gmail.com>
2014-07-08 00:53:21 -0700

    scripts file to configure linux

---
+++ setup_lamp.sh
@@ -0,0 +1,20 @@
+# Make a separate directory called scripts.
+mkdir scripts
+cd scripts
+
+sudo apt-get install git
+
+git init
+
+sudo clone github
+
+
+# Install setuptools
+sudo python ez_setup.py
+
+# Install 
+# Install apache2
+sudo apt-get install apache2
+
+# Install mysql
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
