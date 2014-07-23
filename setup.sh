8721707e082c0714588a34cde2acc2689debbfb5
Partha B Baruah <parthabb@gmail.com>
2014-07-22 22:38:57 -0700

    Installations

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Install setuptools
+wget https://bootstrap.pypa.io/ez_setup.py
+sudo python ez_setup.py
+
+# install pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
---
+++ setup_lamp.sh
@@ -0,0 +1,5 @@
+# Install 
+sudo apt-get install apache2
+
+# Install mysql
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
