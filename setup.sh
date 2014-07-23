93a0a41f9daf582092512eb54c32c17878eafe35
Partha B Baruah <parthabb@gmail.com>
2014-07-22 23:41:02 -0700

    modifications to pip-requires and setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ sudo python ez_setup.py
 # install pip
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
+
+sudo apt-get install vim
+sudo apt-get install g++
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-dev
